3906e5a84af1cad01ef25106e93b9d0b7a7eb9a4
Juan Carlos mamani huayta <carloshuayta112@gmail.com>
2019-11-07 15:48:12 -0400

    implementacion del login animado

---
+++ lib/Screens/Login/styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+DecorationImage backgroundImage = new DecorationImage(
+  image: new ExactAssetImage('assets/login.jpg'),
+  fit: BoxFit.cover,
+);
+
+DecorationImage tick = new DecorationImage(
+  image: new ExactAssetImage('assets/login.png'),
+  fit: BoxFit.fitHeight,
+
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:edge_detection/edge_detection.dart';
+import 'package:procesos_judiciales/Screens/Home/index.dart';
+import 'package:procesos_judiciales/Screens/Login/index.dart';
 
-void main() => runApp(MyApp());
-
+void main() async{
+ // Routes();
+  runApp(MyApp());
+}
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String ruta = "";
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text("esta es laa ruta" + ruta),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _edge();
+    return Container(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Expedientes',
+        initialRoute: 'login',
+        routes: {
+          'login': (BuildContext context) => LoginScreen(),
+          'home': (BuildContext context) => HomeScreen(),
         },
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        theme: ThemeData(primaryColor: Colors.deepPurple[300]),
       ),
     );
-  }
-
-  _edge() async {
-    String cade = await EdgeDetection.detectEdge;
-    setState(() {
-      ruta = cade;
-    });
   }
 }
